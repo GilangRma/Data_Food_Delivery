69833c84b8286c92c72ed23b3e8d8b06e29f2f88
GilangRamaWiarta <96400289+GilangRma@users.noreply.github.com>
2022-02-07 20:26:23 +0800

    Update

--- src/main/java/com/dimata/demo/user/demo_data_user/services/repo/DataProductRepo.java
+++ src/main/java/com/dimata/demo/user/demo_data_user/services/repo/DataProductRepo.java
@@ -2,11 +2,15 @@ package com.dimata.demo.user.demo_data_user.services.repo;
 
 import com.dimata.demo.user.demo_data_user.models.table.DataProduct;
 
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 
 import reactor.core.publisher.Mono;
 
 public interface DataProductRepo extends R2dbcRepository<DataProduct, Long> {
     Mono<DataProduct> findById(long id);
+
+    @Query("SELECT image_product FROM data_product WHERE id_book= $1")
+     Mono<DataProduct> findById(Long id);
     
 }
